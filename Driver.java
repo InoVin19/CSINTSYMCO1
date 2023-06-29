6b774fefe54a31ef231b77b0c506a1541611e12c
InoVin19 <rey_vincent_inocencio@dlsu.edu.ph>
2023-06-29 10:26:08 +0800

    added GUI implementation (TODO: clean code, add text file input)

--- Driver.java
+++ Driver.java
@@ -1,60 +1,116 @@
-//DFS Algorithm reference: https://www.programiz.com/dsa/graph-dfs
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Driver {
-    public static void main(String args[]) {
-    String edge, numOfVertices;
-    String[] input;
-    int src, dest;
-    Graph g = null;
-      
+import java.util.*;
 
-    try{
-      File myFile = new File("test.txt");
-      Scanner scan = new Scanner(myFile);
+public class Driver extends Application {
 
-        if(scan.hasNextLine()){
-          numOfVertices = scan.next();
-            scan.nextLine();
-          g = new Graph(Integer.parseInt(numOfVertices)); // Create a graph with 13 vertices
-        }
+    private Map<String, List<String>> createGraph() {
+        // Create your graph here
+        Map<String, List<String>> graph = new HashMap<>();
 
-        while(scan.hasNextLine()){
-          edge = scan.nextLine();
-          input = edge.split(" ");
+        graph.put("Oradea", Arrays.asList("Zerind", "Sibiu"));
+        graph.put("Zerind", Collections.singletonList("Arad"));
+        graph.put("Sibiu", Arrays.asList("Arad", "Fagaras", "Rimnicu Vilcea"));
+        graph.put("Arad", Arrays.asList("Timisoara"));
+        graph.put("Fagaras", Arrays.asList("Bucharest"));
+        graph.put("Rimnicu Vilcea", Arrays.asList("Pitesti", "Craiova"));
+        graph.put("Timisoara", Arrays.asList("Lugoj"));
+        graph.put("Lugoj", Arrays.asList("Mehadia"));
+        graph.put("Mehadia", Arrays.asList("Dobreta"));
+        graph.put("Dobreta", Arrays.asList("Craiova"));
+        graph.put("Craiova", Arrays.asList("Pitesti"));
+        graph.put("Pitesti", Arrays.asList("Bucharest"));
+        graph.put("Bucharest", Arrays.asList("Giurgiu", "Urziceni"));
+        graph.put("Giurgiu", Arrays.asList());
+        graph.put("Urziceni", Arrays.asList("Vaslui", "Hirsova"));
+        graph.put("Vaslui", Arrays.asList("Lasi"));
+        graph.put("Lasi", Arrays.asList("Neamt"));
+        graph.put("Neamt", Arrays.asList());
+        graph.put("Hirsova", Arrays.asList("Eforie"));
+        graph.put("Eforie", Arrays.asList());
 
-          if(input.length > 1){
-            src = Integer.parseInt(input[0]);
-            dest = Integer.parseInt(input[1]);
-            g.addEdge(src, dest);
-          }
-        }
-        
-          scan.close();
-        }catch(FileNotFoundException e){
-          System.out.println("Error");          
-      e.printStackTrace();
+
+        return graph;
     }
 
-    System.out.println("Following is Depth First Traversal");
+    private TreeItem<String> buildTree(String vertex, Map<String, List<String>> graph, Set<String> explored) {
+        TreeItem<String> node = new TreeItem<>(vertex);
 
-    DFS dfs = new DFS(g);
-    dfs.traverse(1, 5);
+        explored.add(vertex);
 
-    if (!dfs.goalFound) {
-        System.out.println("\nGoal state not found!");
+        List<String> neighbors = graph.get(vertex);
+        if (neighbors != null) {
+            for (String neighbor : neighbors) {
+                if (!explored.contains(neighbor)) {
+                    TreeItem<String> childNode = buildTree(neighbor, graph, explored);
+                    node.getChildren().add(childNode);
+                }
+            }
+        }
+
+        return node;
     }
+
+    private boolean dfsTraversal(TreeItem<String> currentNode, String goal, List<String> path) {
+        currentNode.setExpanded(true);
+
+        String nodeValue = currentNode.getValue();
+        path.add(nodeValue);
+
+        if (nodeValue.equals(goal)) {
+            return true;
+        }
+
+        List<TreeItem<String>> children = currentNode.getChildren();
+        for (TreeItem<String> child : children) {
+            if (dfsTraversal(child, goal, path)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Map<String, List<String>> graph = createGraph();
+        String start = "Sibiu";
+        String goal = "Bucharest";
+
+        TreeItem<String> root = buildTree(start, graph, new HashSet<>());
+        TreeView<String> treeView = new TreeView<>(root);
+
+        Label pathLabel = new Label();
+        pathLabel.setWrapText(true);
+        BorderPane.setAlignment(pathLabel, Pos.CENTER);
+        BorderPane.setMargin(pathLabel, new Insets(10));
+
+        BorderPane rootPane = new BorderPane();
+        rootPane.setCenter(treeView);
+        rootPane.setBottom(pathLabel);
+   
+        Scene scene = new Scene(rootPane, 400, 450);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Depth-First-Search");
+        primaryStage.show();
+
+        List<String> path = new ArrayList<>();
+        if (!dfsTraversal(root, goal, path)){
+            pathLabel.setText("Start state: " + start + "\nGoal State: " + goal + "\n\npath: " + String.join(" -> ", path) + ". \n\nGoal State not found.");
+        } else{
+            pathLabel.setText("Start state: " + start + "\nGoal State: " + goal + "\n\npath: " + String.join(" -> ", path) + ". \n\nGoal State reached.");
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
